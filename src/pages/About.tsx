effca832323b626547251bf647f8d73d77264326
Pawel Mizwa <pawel.mizwa@pragmaticcoders.com>
2023-03-15 13:33:29 +0100

    todo list with typescript

---
+++ src/pages/About.tsx
@@ -0,0 +1,17 @@
+import { Container } from "@material-ui/core";
+
+const About = () => {
+  return (
+    <>
+      <Container>
+        <p style={{ fontSize: "1.3rem" }}>
+          <b>Todo list</b> is an open-source project.
+          This app focuses on the ease of use of a Todo App. Write your todos,
+          change some settings, Enjoy!
+        </p>
+      </Container>
+    </>
+  );
+};
+
+export default About;
