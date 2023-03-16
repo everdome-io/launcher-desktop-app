24aa68a5af3c80740951e096b582c39490f66663
Pawel Mizwa <pawel.mizwa@pragmaticcoders.com>
2023-03-16 17:10:49 +0100

    electron implementation

--- src/hooks/useChangeMenuIcon.tsx
+++
@@ -1,8 +0,0 @@
-import { useMediaQuery } from "@material-ui/core";
-import { MoreVert, MoreHoriz, SvgIconComponent } from "@material-ui/icons";
-
-export default function useChangeMenuIcon(): SvgIconComponent {
-  const Icon: SvgIconComponent = () =>
-    useMediaQuery("(max-width: 768px)") ? <MoreVert /> : <MoreHoriz />;
-  return Icon;
-}
---
+++ src/renderer/App.tsx
@@ -0,0 +1,22 @@
+import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+
+function Hello() {
+  return (
+    <div>
+      <div className="Hello">
+        <button />
+      </div>
+    </div>
+  );
+}
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Hello />} />
+      </Routes>
+    </Router>
+  );
+}
