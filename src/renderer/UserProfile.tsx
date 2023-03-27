a949ae7a89d4bcdc2c20f050297473ac48fd9c90
kwbak <kinga.wigurskabak@pragmaticcoders.com>
2023-03-27 11:18:34 +0200

    EV-44 User profile header

--- src/renderer/UserProfile.tsx
+++ src/renderer/UserProfile.tsx
@@ -2,6 +2,7 @@ import { AppState } from '@interfaces';
 import { FC } from 'react';
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import { ProfileDetails } from './views/ProfileDetails';
+import './UserProfile.css';
 
 const UserProfile: FC<{ state: AppState }> = ({ state }) => {
   return (
--- src/renderer/views/ProfileDetails.tsx
+++ src/renderer/views/ProfileDetails.tsx
@@ -1,10 +1,29 @@
 import { AppState } from '@interfaces';
 import { FC } from 'react';
-
+import profileIcon from 'assets/images/menu-profile-nopicture.png';
+import previewIcon from 'assets/images/preview-icon.png';
+import settingsIcon from 'assets/images/settings-icon.png';
+import './ProfileDetails.css';
 export const ProfileDetails: FC<{ state: AppState }> = ({ state }) => {
   return (
     <div className="container">
-      <h1>Profile</h1>
+      <div className="UserProfileHeader">
+        <div className="UserInfo">
+          <img src={profileIcon} width="40" height="40" alt="" />
+          <div className="UserDetails">
+            <h3>User_3758</h3>
+            <span>Wallet address</span>
+          </div>
+        </div>
+        <div className="SideMenu">
+          <a href="#">
+            <img src={previewIcon} />
+          </a>
+          <a href="#">
+            <img src={settingsIcon} />
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
