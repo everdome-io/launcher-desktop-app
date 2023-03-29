728a76d44ac4bb763961a062207fd2cb3fbc0944
Pawel Mizwa <pawel.mizwa@pragmaticcoders.com>
2023-03-29 15:38:43 +0200

    v1.9.8 error massage

--- src/main/main.ts
+++ src/main/main.ts
@@ -98,7 +98,7 @@ const createWindow = async () => {
     shell.openExternal(edata.url);
     return { action: 'deny' };
   });
-  const feedUrl = `https://user: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@github.com/everdome-io/launcher-desktop-app/releases.atom`;
+  const feedUrl = `https://pawelmizwa-pc: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@github.com/everdome-io/launcher-desktop-app/releases.atom`;
   autoUpdater.setFeedURL({ url: feedUrl, provider: 'generic' });
   const result = await autoUpdater.checkForUpdates();
   console.log('checkForUpdatesAndNotify');
