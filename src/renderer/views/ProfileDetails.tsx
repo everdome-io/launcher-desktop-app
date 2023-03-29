b63cea9130a74c3a3cb19fce19c8a9184fca4f1c
kingawb <128366821+kingawb@users.noreply.github.com>
2023-03-29 13:22:01 +0200

    EV-19 Add logged state (#14)

--- src/renderer/views/ProfileDetails.tsx
+++ src/renderer/views/ProfileDetails.tsx
@@ -1,31 +1,39 @@
 import { AppState } from '@interfaces';
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import profileIcon from 'assets/images/menu-profile-nopicture.png';
 import previewIcon from 'assets/images/preview-icon.png';
 import settingsIcon from 'assets/images/settings-icon.png';
 import iconDOME from 'assets/images/icon_DOME.png';
 import './ProfileDetails.css';
 export const ProfileDetails: FC<{ state: AppState }> = ({ state }) => {
+  const [isLogged, setIsLogged] = useState(false);
+
+  const connectWallet = () => {
+    setIsLogged(true);
+  };
+
   return (
     <div className="container">
-      <div className="UserProfileHeader">
-        <div className="UserInfo">
-          <img src={profileIcon} width="40" height="40" alt="" />
-          <div className="UserDetails">
-            <h3>User_3758</h3>
-            <span>Wallet address</span>
+      {isLogged && (
+        <div className="UserProfileHeader" hidden={!isLogged}>
+          <div className="UserInfo">
+            <img src={profileIcon} width="40" height="40" alt="" />
+            <div className="UserDetails">
+              <h3>User_3758</h3>
+              <span>Wallet address</span>
+            </div>
+          </div>
+          <div className="SideMenu">
+            <a href="#">
+              <img src={previewIcon} />
+            </a>
+            <a href="#">
+              <img src={settingsIcon} />
+            </a>
           </div>
         </div>
-        <div className="SideMenu">
-          <a href="#">
-            <img src={previewIcon} />
-          </a>
-          <a href="#">
-            <img src={settingsIcon} />
-          </a>
-        </div>
-      </div>
-      <div className="Wallet">
+      )}
+      <div className="Wallet" hidden={!isLogged}>
         <div className="WalletItem">
           <h5>Wallet</h5>
           <div className="WalletItemDetails">
@@ -43,20 +51,25 @@ export const ProfileDetails: FC<{ state: AppState }> = ({ state }) => {
         </div>
       </div>
       <div className="MainSection">
-        <div className="EmptyMessage">
-          <p className="InfoText">If you new to decentralized world</p>
-          <a
-            className="CTAButton CreateWallet"
-            href="https://www.okx.com/account/register"
-            target="_blank"
-          >
-            Create Wallet
-          </a>
-          <p className="InfoText">or if you already have a wallet</p>
-          <button className="CTAButton CTAButtonInverse ConnectWallet">
-            Connect Wallet
-          </button>
-        </div>
+        {!isLogged && (
+          <div className="EmptyMessage">
+            <p className="InfoText">If you new to decentralized world</p>
+            <a
+              className="CTAButton CreateWallet"
+              href="https://www.okx.com/account/register"
+              target="_blank"
+            >
+              Create Wallet
+            </a>
+            <p className="InfoText">or if you already have a wallet</p>
+            <button
+              className="CTAButton CTAButtonInverse ConnectWallet"
+              onClick={connectWallet}
+            >
+              Connect Wallet
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
