11d56173f5efca379f377dd4d60d9178e5b86adb
Pawel Mizwa <pawel.mizwa@pragmaticcoders.com>
2023-03-29 11:23:28 +0200

    v1.8.6 new cert

--- .erb/scripts/notarize.js
+++ .erb/scripts/notarize.js
@@ -21,10 +21,10 @@ exports.default = async function notarizeMacos(context) {
 
   const appName = context.packager.appInfo.productFilename;
 
-  // await notarize({
-  //   appBundleId: build.appId,
-  //   appPath: `${appOutDir}/${appName}.app`,
-  //   appleId: process.env.APPLE_ID,
-  //   appleIdPassword: process.env.APPLE_ID_PASS,
-  // });
+  await notarize({
+    appBundleId: build.appId,
+    appPath: `${appOutDir}/${appName}.app`,
+    appleId: process.env.APPLE_ID,
+    appleIdPassword: process.env.APPLE_ID_PASS,
+  });
 };
