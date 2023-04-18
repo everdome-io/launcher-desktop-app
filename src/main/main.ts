54586b9f78ca573a91876a7a3937df7ccc2147e8
kingawb <128366821+kingawb@users.noreply.github.com>
2023-04-18 10:58:03 +0200

    Use electron-store in the renderer process (#36)

--- src/main/main.ts
+++ src/main/main.ts
@@ -455,3 +455,10 @@ ipcMain.on(Channels.acceptTerms, (event) => {
     termsAccepted: true,
   });
 });
+
+ipcMain.on('electron-store-get', async (event, val) => {
+  event.returnValue = store.get(val);
+});
+ipcMain.on('electron-store-set', async (event, key, val) => {
+  store.set(key, val);
+});
