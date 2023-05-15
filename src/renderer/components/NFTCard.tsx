0ce5e0795a4084b99e29db07a2f326948bac2998
kingawb <128366821+kingawb@users.noreply.github.com>
2023-05-15 10:05:54 +0200

    EV-227 Spinner component (#97)

--- src/renderer/components/NFTCard.tsx
+++ src/renderer/components/NFTCard.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './NFTCard.module.css';
+import { Spinner } from './Spinner';
 
 interface NFTCardProps {
   colection: string;
@@ -13,22 +14,32 @@ export const NFTCard: React.FC<NFTCardProps> = ({
   variant,
 }) => {
   const nftList = [styles.nftImg1, styles.nftImg2, styles.nftImg3];
+  const [isLoading, setIsLoading] = React.useState(false);
 
   return (
-    <div className={styles.container}>
-      <div className={styles.nftContainer}>
-        <div className={nftList[(variant - 1) % 3]}></div>
-      </div>
-      <div className={styles.tokenInfo}>
-        <div className={styles.item}>
-          <div className={styles.itemName}>Collection</div>
-          <div className={styles.itemValue}>{colection}</div>
-        </div>
-        <div className={styles.item}>
-          <div className={styles.itemName}>Token ID</div>
-          <div className={styles.itemValue}>{tokenId}</div>
-        </div>
-      </div>
+    <div className={isLoading ? styles.containerStatic : styles.container}>
+      {isLoading ? (
+        <>
+          <p>Your NFT is downloading...</p>
+          <Spinner width={40} />
+        </>
+      ) : (
+        <>
+          <div className={styles.nftContainer}>
+            <div className={nftList[(variant - 1) % 3]}></div>
+          </div>
+          <div className={styles.tokenInfo}>
+            <div className={styles.item}>
+              <div className={styles.itemName}>Collection</div>
+              <div className={styles.itemValue}>{colection}</div>
+            </div>
+            <div className={styles.item}>
+              <div className={styles.itemName}>Token ID</div>
+              <div className={styles.itemValue}>{tokenId}</div>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
